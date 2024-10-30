3c7f76c17f8f0662c28d8f8971c704f5edf29b06
Deepika Gandla <155216833+Deepika-Gandla@users.noreply.github.com>
2024-10-30 14:13:25 +0530

    Create code.py

---
+++ code.py
@@ -0,0 +1,28 @@
+import time
+from datetime import datetime as dt
+
+host_path = "C:\\Windows\\System32\\drivers\\etc\\hosts"  # Adjust path for Mac/Linux if needed
+redirect = "127.0.0.1"
+website_list = ["facebook.com", "www.facebook.com"]
+
+while True:
+    # Set blocking hours (e.g., 7 AM to 10 AM)
+    if dt(dt.now().year, dt.now().month, dt.now().day, 7) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 10):
+        with open(host_path, "r+") as file:
+            content = file.read()
+            for website in website_list:
+                if website in content:
+                    pass
+                else:
+                    file.write(redirect + " " + website + "\n")
+        print("All listed websites are blocked!")
+    else:
+        with open(host_path, "r+") as file:
+            content = file.readlines()
+            file.seek(0)
+            for line in content:
+                if not any(website in line for website in website_list):
+                    file.write(line)
+            file.truncate()
+        print("Websites are unblocked! Have fun!")
+    time.sleep(5)
